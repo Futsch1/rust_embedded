2fa5dc563991a72bc7d2be56e47fbea707e36938
Futsch1 <florian.fetz@googlemail.com>
2023-11-01 08:19:11 +0000

    added devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,19 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  gdb-multiarch \
+  openocd \
+  qemu-system-arm \
+  curl \
+  git \
+  gcc
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+source "$HOME/.cargo/env"
+rustup component add rustfmt
+rustup component add clippy
+cargo install cargo-binutils
+cargo install cargo-generate
+rustup component add llvm-tools-preview
+rustup target add thumbv8m.main-none-eabi
